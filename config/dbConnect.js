dc25457b30881d5a227035cb98b19d358ff703c2
ykn9080 <ykn9080@empal.com>
2021-05-05 15:15:39 +0800

    test

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -26,4 +26,4 @@ mongoose.connect(currentsetting.datasrc, {
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("error", console.error.bind(console, "MongoDB connection error::"));
